7456f8d1ec1c9e2f1d04b80773617250fdfb0c17
Arash Zomorodi <a.zomorodi@KANDA.idea>
2023-12-05 14:27:59 +0330

    Adding Image

--- ImageList/Controllers/ImageViewController.cs
+++ ImageList/Controllers/ImageViewController.cs
@@ -1,8 +1,13 @@
 ﻿using ImageList.Data;
 using ImageList.Models;
+using Microsoft.Ajax.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.VisualBasic;
 using Microsoft.WindowsAzure.Storage.Auth;
+using System.Drawing.Text;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace ImageList.Controllers
@@ -33,15 +38,40 @@ namespace ImageList.Controllers
         //POST
         [HttpPost]
         public IActionResult Add(ImageView obj)
-        { 
-            FileInfo info = new FileInfo(obj.Path);
-            obj.Extension = info.Extension;
-            obj.Name = info.Name;
-            obj.Size = 0;
-            obj.CreationDate = info.CreationTime.ToString();
-            _db.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+        {
+            if (obj.Path!=null)
+            {
+                FileInfo info = new FileInfo(obj.Path);
+                if (info.Exists)
+                {
+                    obj.Extension = info.Extension;
+                    obj.Name = info.Name;
+                    obj.Size = (int)info.Length;
+                    obj.CreationDate = info.CreationTime.ToString();
+                    _db.Add(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+         
+        }
+
+         public IActionResult Show(string id) 
+        {
+            Guid guid = new Guid(id);
+
+            var result = _db.IamgeView
+                .Where(e => e.GUID == guid)
+                .Select(e => e.Path)
+                .FirstOrDefault();
+
+            if (result!=null)
+            {
+                byte[] imageByte = System.IO.File.ReadAllBytes(result);
+                return View(imageByte);
+            }
+            return RedirectToAction("Add");
         }
     }
 }
